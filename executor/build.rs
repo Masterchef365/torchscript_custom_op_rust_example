eeb015eb0ae2ac20ab067c3177252efc0cc67d8b
Duncan Freeman <dfreeman@lucidyne.com>
2021-01-25 13:52:50 -0800

    Check script works

--- executor/build.rs
+++ executor/build.rs
@@ -1,16 +1,9 @@
 fn main() {
-    #[cfg(target_os = "windows")]
-    {
-        let mut current_dir = std::env::current_dir().unwrap();
-        current_dir.pop();
-        current_dir.push("custom_op");
-        current_dir.push("build");
-        current_dir.push("Release");
-        println!("cargo:rustc-link-search={}", current_dir.into_os_string().into_string().unwrap());
-    }
-
-    #[cfg(target_os = "linux")]
-    println!("cargo:rustc-link-search={}", "../custom_op/build/");
-
-    println!("cargo:rustc-link-lib=dylib={}", "test_op");
+    let mut current_dir = std::env::current_dir().unwrap();
+    current_dir.pop();
+    current_dir.push("custom_op");
+    println!("cargo:rustc-link-search={}", current_dir.into_os_string().into_string().unwrap());
+    println!("cargo:rustc-link-search={}", 
+    r"C:\Users\dunca\Documents\torchscript_custom_op_rust_example\custom_op\build\temp.win-amd64-3.8\Release\");
+    println!("cargo:rustc-link-lib={}", "_pvcnn_backend");
 }
--- executor/src/main.rs
+++ executor/src/main.rs
@@ -1,12 +1,7 @@
 use anyhow::Result;
 use tch::{IValue, Tensor};
 
-extern "C" {
-    fn test_library();
-}
-
 fn main() -> Result<()> {
-    unsafe { test_library() };
     let device = match tch::Cuda::is_available() {
         true => tch::Device::Cuda(1),
         false => {
@@ -18,15 +13,18 @@ fn main() -> Result<()> {
     let module = tch::CModule::load_on_device("../custom_op/cell.zip", device)?;
     let kind = (tch::Kind::Float, device);
     for _ in 0..10_000 {
-        let x = Tensor::rand(&[3, 4], kind);
-        let h = Tensor::rand(&[3, 4], kind);
+        let x = Tensor::rand(&[3, 2, 2], kind);
+        let h = Tensor::rand(&[3, 2, 2], kind);
         let output = module.forward_is(&[IValue::Tensor(x), IValue::Tensor(h)])?;
+        dbg!(output);
+        /*
         if let IValue::Tuple(mut v) = output {
             if let (Some(IValue::Tensor(a)), Some(IValue::Tensor(b))) = (v.pop(), v.pop()) {
                 a.print();
                 b.print();
             }
         }
+        */
     }
 
     Ok(())
