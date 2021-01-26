b0145e13944bf5fc54b47354f8c56e299cd5dbbe
Duncan Freeman <dfreeman@lucidyne.com>
2021-01-25 16:35:41 -0800

    Slightly more reliable build path

--- custom_op/compile_ops.py
+++ custom_op/compile_ops.py
@@ -1,5 +1,4 @@
 import os
-_src_path = os.path.dirname(os.path.abspath(__file__))
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 setup(
@@ -7,19 +6,9 @@ setup(
         ext_modules=[
             CUDAExtension(name='_pvcnn_backend',
                 extra_cflags=['-O3', '-std=c++17'],
-                sources=[os.path.join(_src_path,'src', f) for f in [
+                sources=[os.path.join('src', f) for f in [
                     'ball_query/ball_query.cpp',
                     'ball_query/ball_query_kern.cu',
-                    #'grouping/grouping.cpp',
-                    #'grouping/grouping_kern.cu',
-                    #'interpolate/neighbor_interpolate.cpp',
-                    #'interpolate/neighbor_interpolate_kern.cu',
-                    #'interpolate/trilinear_devox.cpp',
-                    #'interpolate/trilinear_devox_kern.cu',
-                    #'sampling/sampling.cpp',
-                    #'sampling/sampling_kern.cu',
-                    #'voxelization/vox.cpp',
-                    #'voxelization/vox_kern.cu',
                     'bindings.cpp',
                 ]]
                 )
